33c006f9bffec5d603bc88411dd4d30424621e49
stephanminges <stephanminges@gmail.com>
2023-03-14 21:44:45 -0500

    Made it typesafe and robust

--- src/index.ts
+++ src/index.ts
@@ -1,27 +1,33 @@
-type CallbackFunction = (...args: any[]) => any;
+type TryGuardResult<T, E extends Error = Error> = [E | null, T | null];
+type Callback<T, A extends any[]> = (...args: A) => T;
+type CallbackPromise<T, A extends any[]> = (...args: A) => Promise<T>;
 
-export function TryGuard(callback: CallbackFunction, ...args: any[]) {
-  let data = null;
-  let err = null;
+export function TryGuard<T, A extends any[] = any[], E extends Error = Error>(
+  callback: Callback<T, A>,
+  args: A
+): TryGuardResult<T, E> {
+  let data: T | null = null;
+  let err: E | null = null;
   try {
     const res = callback(...args);
     data = res;
-  } catch (e) {
+  } catch (e: any) {
     err = e;
   }
   return [err, data];
 }
 
-export async function TryGuardAsync(
-  callback: CallbackFunction,
-  ...args: any[]
-) {
-  let data = null;
-  let err = null;
+export async function TryGuardAsync<
+  T,
+  A extends any[] = any[],
+  E extends Error = Error
+>(callback: CallbackPromise<T, A>, args: A): Promise<TryGuardResult<T, E>> {
+  let data: T | null = null;
+  let err: E | null = null;
   try {
     const res = await callback(...args);
     data = res;
-  } catch (e) {
+  } catch (e: any) {
     err = e;
   }
   return [err, data];
--- test/indext.test.ts
+++ test/indext.test.ts
@@ -1,24 +1,34 @@
 import { expect, it } from "vitest";
 import { TryGuard, TryGuardAsync } from "../src";
 
-it("tryguard should handle error", () => {
-  const [err, data] = TryGuard((str: string) => str.split(" "), 1);
-  expect(err !== null).toBe(true);
-  expect(data).toBe(null);
-});
-
 it("tryguard should handle synchronous operations successfully", () => {
-  const [err, data] = TryGuard((str: string) => str.split(" "), "test");
+  function splitter(str: string) {
+    return str.split(" ");
+  }
+  const [err, data] = TryGuard(splitter, ["test"]);
+
   expect(err).toBe(null);
   expect(Array.isArray(data)).toBe(true);
 });
 
 it("tryguard should handle asynchronous operations successfully ", async () => {
-  const [err, data] = await TryGuardAsync(async (endpoint: string) => {
+  async function gettSme(endpoint: string) {
     const response = await fetch(endpoint);
     return response.json();
-  }, "https://jsonplaceholder.typicode.com/posts");
+  }
+  const [err, data] = await TryGuardAsync(gettSme, [
+    "https://jsonplaceholder.typicode.com/posts",
+  ]);
 
   expect(err).toBe(null);
   expect(Array.isArray(data)).toBe(true);
+});
+it("tryguard should handle errors successfully ", async () => {
+  async function gettSme() {
+    throw new Error("Something went wrong");
+  }
+  const [err, data] = await TryGuardAsync(gettSme, []);
+
+  expect(err !== null).toBe(true);
+  expect(data == null).toBe(true);
 });
